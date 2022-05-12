62ef91e4f775a729e9c368f2e716542986d30add
Rinkesh-PG <rinkesh@propertyguru.tech>
2022-05-12 11:41:34 +0530

    [main] Added redux store and reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
 import Login from "./pages/Login";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <div>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/wallet" element={<Wallet />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/login" element={<Login />} />
         </Routes>
