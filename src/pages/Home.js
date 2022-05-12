c08356776483ab7785592bbb941e4b39df21a386
xandernoob <alexander_kobebryan@hotmail.com>
2022-05-12 18:11:11 +0800

    WIP for image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Box, Container, Tab, Tabs } from "@mui/material";
+import { Box, Container, Grid, Paper, Tab, Tabs } from "@mui/material";
 import axios from "axios";
 
 import HomeTabPanel from "../modules/home/components/HomeTabPanel";
+import image1 from "../image-1.png";
+import image2 from "../image-2.png";
+import image3 from "../image-3.png";
+import * as url from "url";
 
 const a11yProps = index => {
   return {
@@ -24,8 +28,6 @@ const Home = () => {
     getData();
   }, []);
 
-  console.log(initialData);
-
   const handleChange = (event, val) => {
     setValue(val);
   };
@@ -38,15 +40,38 @@ const Home = () => {
       }}
     >
       <a href="/wallet">Connect Wallet</a>
-      <Box></Box>
+
       <Container maxWidth="md">
+        <Box sx={{ flexGrow: 1, overflow: "hidden" }}>
+          <Grid container spacing={1}>
+            <Grid item xs={4} md={8}>
+              <Box sx={{ width: "100%" }}>
+                <img style={{ width: "100%" }} src={image1} alt="Listing" />
+              </Box>
+            </Grid>
+            <Grid item xs={4} md={2}>
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  backgroundImage: `url(${image3})`,
+                }}
+              ></Box>
+            </Grid>
+            <Grid item xs={4} md={2}>
+              <Box>
+                <img style={{ width: "100%" }} src={image2} alt="Map" />
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
         <Box sx={{ width: "100%" }}>
           <Box>
             <Tabs
               value={value}
               onChange={handleChange}
-              textColor="white"
               aria-label="basic tabs example"
+              textColor={"inherit"}
             >
               <Tab label="Details" {...a11yProps(0)} />
               <Tab label="Bids" {...a11yProps(1)} />
